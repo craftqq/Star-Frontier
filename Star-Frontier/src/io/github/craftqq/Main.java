e193ef828ad2ccfe1b692e2a4ea27423e0713a96
awsdcrafting <scisneromam@t-online.de>
2015-03-29 14:27:28 +0200

    mynewclass

--- Star-Frontier/src/io/github/craftqq/Main.java
+++ Star-Frontier/src/io/github/craftqq/Main.java
@@ -1,10 +1,13 @@
 package io.github.craftqq;
 
+import io.github.awsdcrafting.Setup;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Setup.doStuff();
 	}
 
 }
