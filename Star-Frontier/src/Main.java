98f8941c43f17ca2621da7e32d81452204aa0fd0
craftqq <nimdrop@t-online.de>
2015-03-30 17:20:42 +0200

    erstes Fenster!

--- Star-Frontier/src/Main.java
+++ Star-Frontier/src/Main.java
@@ -5,13 +5,15 @@ import io.github.awsdcrafting.Setup;
 import io.github.craftqq.StartFenster;
 
 import javax.swing.*;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 public class Main 
 {
 	private static JFrame fenster;
 	
 	public static void main(String[] args) 
-	{
+	{        
+		System.setProperty("swing.defaultlaf", "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 		Message.printMessage();
 		Setup.load();
 		fenster = new StartFenster();
--- Star-Frontier/src/io/github/craftqq/StartFenster.java
+++ Star-Frontier/src/io/github/craftqq/StartFenster.java
@@ -1,14 +1,69 @@
 package io.github.craftqq;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class StartFenster extends JFrame 
 {
-
+	private static final long serialVersionUID = 155551L;
+	
+	private JMenuBar menuBar;
+	
 	public StartFenster()
 	{
-		
-		
+        this.setTitle("Star Frontier");
+        this.setSize(800, 600);
+        generateMenu();
+        this.setJMenuBar(menuBar);
+        
+        JPanel contentPane = new JPanel(null);
+        contentPane.setPreferredSize(new Dimension(800,600));
+        contentPane.setBackground(new Color(192,192,192));
+
+        this.add(contentPane);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.pack();
+        this.setVisible(true);
 	}
 	
+    private void generateMenu()
+    {
+        menuBar = new JMenuBar();
+
+        JMenu file = new JMenu("File");
+        JMenu tools = new JMenu("Tools");
+        JMenu help = new JMenu("Help");
+        
+        JMenuItem open = new JMenuItem("Open   ");
+        open.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){
+        System.out.println("Hier ist noch nichts.");}});
+        JMenuItem save = new JMenuItem("Save   ");
+        save.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){
+        System.out.println("Hier ist noch nichts.");}});
+        JMenuItem exit = new JMenuItem("Exit   ");
+        exit.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){
+        System.exit(0);}});
+        JMenuItem preferences = new JMenuItem("Preferences   ");
+        preferences.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){
+        System.out.println("Hier ist noch nichts.");}});
+        JMenuItem about = new JMenuItem("About   ");
+        about.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){
+        System.out.println("Dies ist ein Weltraumstrategiespiel.");
+        System.out.println("Version: ");}});
+
+        
+        file.add(open);
+        file.add(save);
+        file.addSeparator();
+        file.add(exit);
+        tools.add(preferences);
+        help.add(about);
+
+        menuBar.add(file);
+        menuBar.add(tools);
+        menuBar.add(help);
+    }
+
 }
