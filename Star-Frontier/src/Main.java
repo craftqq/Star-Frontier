4273d3ff9621adb7ec3a5290ff1690c7bd1bb2bd
craftqq <nimdrop@t-online.de>
2015-03-30 17:01:10 +0200

    removed hello world

--- Star-Frontier/src/Main.java
+++ Star-Frontier/src/Main.java
@@ -10,7 +10,6 @@ public class Main
 	private static JFrame fenster;
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
 		Setup.load();
 		fenster = new StartFenster();
 		System.out.println(String.valueOf(Setup.music));
