6089abd3ecb4bf0594b24757d20b0224c323be92
awsdcrafting <scisneromam@t-online.de>
2015-03-30 16:49:52 +0200

    settings now

--- Star-Frontier/src/Main.java
+++ Star-Frontier/src/Main.java
@@ -11,7 +11,7 @@ public class Main
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
-		Setup.doStuff();
+		Setup.load();
 		fenster = new StartFenster();
 	}
 
--- Star-Frontier/src/io/github/awsdcrafting/Setup.java
+++ Star-Frontier/src/io/github/awsdcrafting/Setup.java
@@ -1,11 +1,17 @@
 package io.github.awsdcrafting;
 
+import io.github.craftqq.DateiLeser;
+
 public class Setup
 {
+	public static boolean sound;
+	public static boolean music;
 	
-	public static void doStuff()
+	public static void load()
 	{
 		System.out.println("Welcome to Star Frontier!");
+		String[] settings = DateiLeser.leseDatei("settings.txt");
+		
 	}
 
 }
