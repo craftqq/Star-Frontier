452e50a8f46b91bb04a30c9c8ac7b519a09e1302
craftqq <nimdrop@t-online.de>
2015-04-03 20:15:56 +0200

    +class Waffe

--- Star-Frontier/src/io/github/craftqq/waffen/Waffe.java
+++ Star-Frontier/src/io/github/craftqq/waffen/Waffe.java
@@ -2,5 +2,14 @@ package io.github.craftqq.waffen;
 
 public abstract class Waffe 
 {
-
+	int type;
+	public static int WEAPON_NONE = 0;
+	public static int WEAPON_CANNON = 1;
+	public static int WEAPON_ROCKET = 2;
+	public static int WEAPON_BOMB = 3;
+	
+	public int gibType()
+	{
+		return type;
+	}
 }
