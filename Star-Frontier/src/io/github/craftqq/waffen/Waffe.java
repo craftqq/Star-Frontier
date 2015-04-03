e98f936d376ca303a3343100930672e672aede2f
craftqq <nimdrop@t-online.de>
2015-04-03 20:19:33 +0200

    *finalised weapon type ints

--- Star-Frontier/src/io/github/craftqq/waffen/Waffe.java
+++ Star-Frontier/src/io/github/craftqq/waffen/Waffe.java
@@ -3,10 +3,10 @@ package io.github.craftqq.waffen;
 public abstract class Waffe 
 {
 	int type;
-	public static int WEAPON_NONE = 0;
-	public static int WEAPON_CANNON = 1;
-	public static int WEAPON_ROCKET = 2;
-	public static int WEAPON_BOMB = 3;
+	public static final int WEAPON_NONE = 0;
+	public static final int WEAPON_CANNON = 1;
+	public static final int WEAPON_ROCKET = 2;
+	public static final int WEAPON_BOMB = 3;
 	
 	public int gibType()
 	{
