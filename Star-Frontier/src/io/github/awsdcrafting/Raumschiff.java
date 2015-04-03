e27a32b5df034e1eccaeaeff444c440f391902b5
awsdcrafting <scisneromam@t-online.de>
2015-04-03 19:22:55 +0200

    class raumschiff geändert

--- Star-Frontier/src/io/github/awsdcrafting/Raumschiff.java
+++ Star-Frontier/src/io/github/awsdcrafting/Raumschiff.java
@@ -5,4 +5,8 @@ import io.github.craftqq.waffen.WaffenSlot;
 public class Raumschiff 
 {
 	private WaffenSlot[] waffenslots;
+	private int armour;
+	private int speedMultiplier;
+	private int cargoSpace;
+	private boolean cantransportcargo;
 }
