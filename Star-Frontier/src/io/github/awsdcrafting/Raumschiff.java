36193efaad4600ac991672080bfb3cf934e99ac1
craftqq <nimdrop@t-online.de>
2015-04-03 20:08:42 +0200

    +constructor in class Raumschiff +getter methods in Raumschiff

--- Star-Frontier/src/io/github/awsdcrafting/Raumschiff.java
+++ Star-Frontier/src/io/github/awsdcrafting/Raumschiff.java
@@ -4,9 +4,40 @@ import io.github.craftqq.waffen.WaffenSlot;
 
 public class Raumschiff 
 {
-	private WaffenSlot[] waffenslots;
-	private int armour;
-	private int speedMultiplier;
-	private int cargoSpace;
-	private boolean cantransportcargo;
+	private WaffenSlot[] waffenSlots;
+	private int armor;
+	private int interiorSpace;
+	private boolean canTransportCargo;
+	
+	public Raumschiff(int anzahlWaffen, int armor_, int interiorSpace_, boolean canTransportCargo_)
+	{
+		waffenSlots = new WaffenSlot[anzahlWaffen];
+		armor = armor_;
+		interiorSpace = interiorSpace_;
+		canTransportCargo = canTransportCargo_;
+	}
+	
+	public boolean kannCargoTransportieren()
+	{
+		return canTransportCargo;
+	}
+	
+	public int gibInteriorSpace()
+	{
+		return interiorSpace;
+	}
+	
+	public int gibarmor()
+	{
+		return armor;
+	}
+	
+	public int gibAnzahlWaffenSlots()
+	{
+		return waffenSlots.length;
+	}
+	public WaffenSlot gibWaffenSlot(int slot)
+	{
+		return waffenSlots[slot];
+	}
 }
