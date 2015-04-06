329afc57590e4904f550b1d10efeae88a3f1a35f
awsdcrafting <scisneromam@t-online.de>
2015-04-06 14:01:10 +0200

    +passivwaffenclass

---
+++ Star-Frontier/src/io/github/awsdcrafting/passiveObjects/Waffen.java
@@ -0,0 +1,13 @@
+package io.github.awsdcrafting.passiveObjects;
+
+import io.github.craftqq.passiveObjects.PassiveWaffe;
+
+public class Waffen 
+{
+	public static PassiveWaffe laser;
+	public static PassiveWaffe neutrinogeschuetz;
+	public static PassiveWaffe javaRakete;
+	public static PassiveWaffe leechRakete;
+	public static PassiveWaffe stingrayRakete;
+	public static PassiveWaffe nheRakete;
+}
