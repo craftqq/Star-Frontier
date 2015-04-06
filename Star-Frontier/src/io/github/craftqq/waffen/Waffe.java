4f7f92a08d83e6350b9aefbe21b429ff4337fb7c
craftqq <nimdrop@t-online.de>
2015-04-06 11:49:26 +0200

    *reverted last changes to class Waffe *reverted last changes to class WaffenSlot *added lockedType to class WaffenSlot

--- Star-Frontier/src/io/github/craftqq/waffen/Waffe.java
+++ Star-Frontier/src/io/github/craftqq/waffen/Waffe.java
@@ -2,12 +2,16 @@ package io.github.craftqq.waffen;
 
 public abstract class Waffe 
 {
+	protected int type;
 	public static final int WEAPON_NONE = 0;
 	public static final int WEAPON_CANNON = 1;
 	public static final int WEAPON_ROCKET = 2;
 	public static final int WEAPON_BOMB = 3;
 	
-	public abstract int gibType();
+	public int gibType()
+	{
+		return type;
+	}
 	
 	public abstract boolean hasReloaded();
 	
--- Star-Frontier/src/io/github/craftqq/waffen/WaffenSlot.java
+++ Star-Frontier/src/io/github/craftqq/waffen/WaffenSlot.java
@@ -1,11 +1,37 @@
 package io.github.craftqq.waffen;
 
-public interface WaffenSlot 
+public class WaffenSlot 
 {
+	private int maxClass;
+	private int size;
+	private boolean teilbar;
+	protected int lockedType;
 	
-	public int gibSize();
+	public WaffenSlot(int maxClass_, int size_, boolean teilbar_)
+	{
+		maxClass = maxClass_;
+		size = size_;
+		teilbar = teilbar_;
+		lockedType = 0;
+	}
 	
-	public boolean istTeilbar();
+	public int gibKlasse()
+	{
+		return maxClass;
+	}
 	
-	public int gibType();
+	public int gibSize()
+	{
+		return size;
+	}
+	
+	public boolean istTeilbar()
+	{
+		return teilbar;
+	}
+	
+	public int gibType()
+	{
+		return lockedType;
+	}
 }
