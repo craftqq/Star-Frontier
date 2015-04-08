abd0b253244b5b0fa933fa20843bdc31effa925d
awsdcrafting <scisneromam@t-online.de>
2015-04-08 12:23:01 +0200

    neue passive waffen

--- Star-Frontier/src/io/github/awsdcrafting/passiveObjects/Waffen.java
+++ Star-Frontier/src/io/github/awsdcrafting/passiveObjects/Waffen.java
@@ -5,7 +5,12 @@ import io.github.craftqq.passiveObjects.PassiveWaffe;
 public class Waffen 
 {
 	public static PassiveWaffe laser;
-	public static PassiveWaffe neutrinogeschuetz;
+	public static PassiveWaffe neutrinoGeschuetz;
+	public static PassiveWaffe plasmaKanone;
+	public static PassiveWaffe uranGeschuetz;
+	public static PassiveWaffe bombe250KG;
+	public static PassiveWaffe bombe500KG;
+	public static PassiveWaffe torpedo;
 	public static PassiveWaffe javaRakete;
 	public static PassiveWaffe leechRakete;
 	public static PassiveWaffe stingrayRakete;
