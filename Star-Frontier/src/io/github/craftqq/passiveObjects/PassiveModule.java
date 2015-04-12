e85ee521044c75dc9ec0799076cabaabb7aaa9ac
craftqq <nimdrop@t-online.de>
2015-04-12 13:05:56 +0200

    +class PassiveModule (tbd)

---
+++ Star-Frontier/src/io/github/craftqq/passiveObjects/PassiveModule.java
@@ -0,0 +1,30 @@
+package io.github.craftqq.passiveObjects;
+
+public class PassiveModule implements BuyablePassiveObject,
+		ResearchablePassiveObject {
+
+	@Override
+	public int anzahlResearchPoints() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int researchKlasse() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Preis gibPreis() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String gibName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
