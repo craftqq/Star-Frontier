1d4fba9acb55f1515b29f5de1aaab135df350625
craftqq <nimdrop@t-online.de>
2015-05-23 20:30:02 +0200

    +ChatTextArea in JElementHelper +initial class chat(empty for now)

--- Star-Frontier/src/io/github/craftqq/GUI/JElementHelper.java
+++ Star-Frontier/src/io/github/craftqq/GUI/JElementHelper.java
@@ -1,9 +1,21 @@
 package io.github.craftqq.GUI;
 
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.event.ActionListener;
 
-import java.awt.*;
-import java.awt.event.*;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.Element;
 
 public class JElementHelper 
 {
@@ -48,6 +60,22 @@ public class JElementHelper
         return eingabe;
     }
     
+    public static JTextArea baueChatStyleTextArea(String text, int x, int y)
+    {
+        ChatTextArea ausgabe = new ChatTextArea();
+        ausgabe.setBounds(x,y,140,35);
+        ausgabe.setBackground(new Color(255,255,255));
+        ausgabe.setForeground(new Color(0,0,0));
+        ausgabe.setEnabled(true);
+        ausgabe.setFont(new Font("SansSerif",0,12));
+        ausgabe.setText(text);
+        ausgabe.setBorder(BorderFactory.createBevelBorder(1));
+        ausgabe.setEditable(false);
+        ausgabe.fill();
+        ausgabe.setVisible(true);
+        return ausgabe;
+    }
+    
     public static JTextField baueTextField(String text, int x, int y)
     {
     	JTextField feld = new JTextField();
@@ -85,4 +113,47 @@ public class JElementHelper
         list.setVisible(true);
         return list;
     }
+}
+class ChatTextArea extends JTextArea
+{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4538836711452941905L;
+
+	public void fill()
+	{
+		Font f = this.getFont();
+		Graphics g = this.getGraphics();
+		FontMetrics fm = g.getFontMetrics(f);
+		int i = fm.getHeight();
+		int lines =(int) Math.ceil(((double)this.getHeight())/((double)i));
+		String s = "";
+		for(int j = 0; j<=lines;j++)
+		{
+			s = s.concat("\n");
+		}
+		this.setText(s);
+	}
+	
+	@Override
+	public void append(String s)
+	{
+        Document doc = getDocument();
+        if (doc != null) 
+        {
+            try 
+            {
+            	Element root = doc.getDefaultRootElement();
+            	Element first = root.getElement(0);
+            	doc.remove(0, first.getEndOffset());
+                doc.insertString(doc.getLength(), s, null);
+            }
+            catch (BadLocationException e) 
+            {
+            	
+            }
+        }
+	}
 }
---
+++ Star-Frontier/src/io/github/craftqq/NetIO/Chat.java
@@ -0,0 +1,6 @@
+package io.github.craftqq.NetIO;
+
+public interface Chat 
+{
+	public void send(String message);
+}
